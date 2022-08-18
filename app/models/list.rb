7c23a8b800033b33bf7a5c8b3f6a70cdf80d7800
johnson-darko <johnsondarko365@gmail.com>
2022-08-18 15:34:52 +0200

    validation done

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,5 @@
 class List < ApplicationRecord
-	
+	has_many :movies, through: :bookmarks, dependent: :destroy
+	validates :name, uniqueness: true, presence: true
+	has_many :bookmarks, dependent: :destroy
 end
